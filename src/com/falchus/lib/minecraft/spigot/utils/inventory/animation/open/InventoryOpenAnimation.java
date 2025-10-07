a23a81cacea3e3d02d12b6f56fd25da4c824de69
Elias <elias@falchus.com>
2025-10-07 16:47:30 +0200

    animations

--- src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/InventoryOpenAnimation.java
+++ src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/InventoryOpenAnimation.java
@@ -1,6 +1,5 @@
 package com.falchus.lib.minecraft.spigot.utils.inventory.animation.open;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -44,13 +43,7 @@ public abstract class InventoryOpenAnimation {
 					return;
 				}
 				
-				ItemStack item = items[tick];
-				boolean skip = item == null
-						|| item.getType() == Material.AIR
-						|| (item.hasItemMeta() && (item.getItemMeta().getDisplayName().equals("§r") || item.getItemMeta().getDisplayName().isEmpty()));
-				if (!skip) {
-					animate(player, inventory, items, tick);	
-				}
+				animate(player, inventory, items, tick);
 				tick++;
 			}
 		}.runTaskTimer(plugin, 0, delayTicks);
