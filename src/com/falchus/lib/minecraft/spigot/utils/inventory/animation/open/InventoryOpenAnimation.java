01e20a72fdc2d22c5fdec064a2570ec418bb02a5
Elias <elias@falchus.com>
2025-10-07 16:38:39 +0200

    yay animations

--- src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/InventoryOpenAnimation.java
+++ src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/InventoryOpenAnimation.java
@@ -3,6 +3,7 @@ package com.falchus.lib.minecraft.spigot.utils.inventory.animation.open;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.falchus.lib.minecraft.spigot.FalchusLibMinecraftSpigot;
 
@@ -12,13 +13,13 @@ import lombok.Getter;
 public abstract class InventoryOpenAnimation {
 	
 	protected final FalchusLibMinecraftSpigot plugin = FalchusLibMinecraftSpigot.getInstance();
-	protected int delayTicks = 2;
+	protected int delayTicks = 1;
 	
 	/**
 	 * Called to implement animation logic.
 	 * This will be scheduled automatically after delayTicks.
 	 */
-	protected abstract void animate(Player player, Inventory inventory, ItemStack[] items);
+	protected abstract void animate(Player player, Inventory inventory, ItemStack[] items, int tick);
 	
 	/**
 	 * Plays the animation with automatic scheduling using delayTicks.
@@ -26,6 +27,25 @@ public abstract class InventoryOpenAnimation {
 	public final void play(Player player, Inventory inventory) {
 		ItemStack[] items = inventory.getContents();
 		inventory.clear();
-		animate(player, inventory, items);
+		
+		new BukkitRunnable() {
+			int tick = 0;
+			
+			@Override
+			public void run() {
+				if (!player.getOpenInventory().getTopInventory().equals(inventory)) {
+					cancel();
+					return;
+				}
+				
+				if (tick >= items.length) {
+					cancel();
+					return;
+				}
+				
+				animate(player, inventory, items, tick);
+				tick++;
+			}
+		}.runTaskTimer(plugin, 0, delayTicks);
     }
 }
--- src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/impl/ItemAppearAnimation.java
+++ src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/impl/ItemAppearAnimation.java
@@ -4,7 +4,6 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import com.falchus.lib.minecraft.spigot.utils.inventory.animation.open.InventoryOpenAnimation;
 
@@ -15,24 +14,11 @@ public class ItemAppearAnimation extends InventoryOpenAnimation {
 	}
 	
 	@Override
-	protected void animate(Player player, Inventory inventory, ItemStack[] items) {
-		new BukkitRunnable() {
-			int i = 0;
-			
-			@Override
-			public void run() {
-				if (i >= items.length) {
-					cancel();
-					return;
-				}
-				
-				ItemStack item = items[i];
-				if (item != null) {
-					inventory.setItem(i, item);
-					player.playSound(player.getLocation(), Sound.CHICKEN_EGG_POP, 1, 2);
-				}
-				i++;
-			}
-		}.runTaskTimer(plugin, 0, delayTicks);
+	protected void animate(Player player, Inventory inventory, ItemStack[] items, int tick) {
+		ItemStack item = items[tick];
+		if (item != null) {
+			inventory.setItem(tick, item);
+			player.playSound(player.getLocation(), Sound.CHICKEN_EGG_POP, 1, 2);
+		}
 	}
 }
