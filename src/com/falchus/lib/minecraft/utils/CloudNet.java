82b3dc031d7502946f91fca8908a0bcd3a8a1aae
Elias <elias@falchus.com>
2025-10-15 17:59:08 +0200

    added CloudNet SyncProxy MOTD stuff

--- src/com/falchus/lib/minecraft/utils/CloudNet.java
+++ src/com/falchus/lib/minecraft/utils/CloudNet.java
@@ -3,6 +3,7 @@ package com.falchus.lib.minecraft.utils;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 import eu.cloudnetservice.driver.inject.InjectionLayer;
 import eu.cloudnetservice.driver.provider.CloudServiceFactory;
@@ -14,6 +15,10 @@ import eu.cloudnetservice.driver.service.ServiceInfoSnapshot;
 import eu.cloudnetservice.modules.bridge.BridgeServiceHelper;
 import eu.cloudnetservice.modules.bridge.player.CloudPlayer;
 import eu.cloudnetservice.modules.bridge.player.PlayerManager;
+import eu.cloudnetservice.modules.syncproxy.config.SyncProxyConfiguration;
+import eu.cloudnetservice.modules.syncproxy.config.SyncProxyLoginConfiguration;
+import eu.cloudnetservice.modules.syncproxy.config.SyncProxyMotd;
+import eu.cloudnetservice.modules.syncproxy.impl.node.NodeSyncProxyManagement;
 import eu.cloudnetservice.wrapper.holder.ServiceInfoHolder;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
@@ -30,6 +35,7 @@ public class CloudNet {
 	public static final PlayerManager playerManager = InjectionLayer.ext().instance(ServiceRegistry.class).defaultInstance(PlayerManager.class);
 	public static final CloudServiceFactory cloudServiceFactory = InjectionLayer.ext().instance(CloudServiceFactory.class);
 	public static final CloudServiceProvider cloudServiceProvider = InjectionLayer.ext().instance(CloudServiceProvider.class);
+	public static final NodeSyncProxyManagement syncProxyManagement = InjectionLayer.ext().instance(NodeSyncProxyManagement.class);
 	
 	/**
 	 * Broadcasts a message to all players globally.
@@ -65,7 +71,7 @@ public class CloudNet {
 	/**
 	 * Gets the player count for the group.
 	 */
-	public static int getPlayerCountFromGroup(String group) {
+	public static int getPlayerCountFromGroup(@NonNull String group) {
 		Collection<CloudPlayer> players = playerManager.onlinePlayers().players();
 		Collection<ServiceInfoSnapshot> services = cloudServiceProvider.servicesByGroup(group);
 		int playerCount = 0;
@@ -76,5 +82,46 @@ public class CloudNet {
 									.count();
 		}
 		return playerCount;
+	}
+	
+	/**
+	 * Sets MOTD for the node and group
+	 */
+	public static void setMOTD(@NonNull String nodeUUID, @NonNull String group, String line1, String line2) {
+		SyncProxyLoginConfiguration loginConfig = getLoginConfigForGroup(nodeUUID, group);
+		
+		updateSyncProxyConfig(builder -> builder.modifyLoginConfigurations(logins -> {
+			SyncProxyMotd motd = loginConfig.motds().stream().findFirst().get();
+			SyncProxyMotd.Builder motdBuilder = SyncProxyMotd.builder(motd);
+			
+			SyncProxyLoginConfiguration.Builder configBuilder = SyncProxyLoginConfiguration.builder(loginConfig);
+			SyncProxyLoginConfiguration config = configBuilder
+				.modifyMotd(motds -> motds.add(motdBuilder
+					.firstLine(line1)
+					.secondLine(line2)
+				.build()))
+			.build();
+			logins.remove(config);
+			logins.add(config);
+		}));
+	}
+	
+	/**
+	 * Gets the SyncProxyLoginConfiguration for the group.
+	 */
+	public static SyncProxyLoginConfiguration getLoginConfigForGroup(@NonNull String nodeUUID, @NonNull String group) {
+		SyncProxyConfiguration config = SyncProxyConfiguration.configurationFromNode(nodeUUID);
+		return config.loginConfigurations().stream()
+			.filter(loginConfig -> loginConfig.targetGroup().equals(group))
+			.findFirst()
+			.get();
+	}
+	
+	/**
+	 * Updates SyncProxyConfiguration
+	 */
+	public static void updateSyncProxyConfig(@NonNull Consumer<SyncProxyConfiguration.Builder> modifier) {
+		modifier.andThen(builder -> syncProxyManagement.configuration(builder.build()))
+			.accept(SyncProxyConfiguration.builder(syncProxyManagement.configuration()));
 	}
 }
